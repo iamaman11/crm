b2b4e1bcd845b15961ce639b2f33e7e7aa97aee4
iamaman11 <44100369+iamaman11@users.noreply.github.com>
2026-07-10 21:00:40 +0300

    feat: roll back outbox deduplication identity

--- database/migrations/0002_typed_payload_encoding.down.sql
+++ database/migrations/0002_typed_payload_encoding.down.sql
@@ -3,6 +3,8 @@ BEGIN;
 ALTER TABLE crm.idempotency_records DROP COLUMN response_payload_encoding;
 ALTER TABLE crm.module_state DROP COLUMN payload_encoding;
 ALTER TABLE crm.workflow_runs DROP COLUMN state_encoding;
+DROP INDEX crm.outbox_deduplication_idx;
+ALTER TABLE crm.outbox_events DROP COLUMN deduplication_key;
 ALTER TABLE crm.outbox_events DROP COLUMN payload_encoding;
 ALTER TABLE crm.records DROP COLUMN payload_encoding;
 
