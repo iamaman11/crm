20a3727d700d09d80abdffbb18400894b3f3ed8d
iamaman11 <44100369+iamaman11@users.noreply.github.com>
2026-07-28 23:17:15 +0300

    Add processing restriction rollback migration

---
+++ database/migrations/0104_customer_privacy_processing_restriction.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS customer_privacy.processing_restriction_idempotency;
+DROP TABLE IF EXISTS customer_privacy.processing_restriction_events;
+DROP TABLE IF EXISTS customer_privacy.processing_restrictions;
